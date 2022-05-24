e2d0ab15070a82000e052ca35792cecf4f4fefe2
N00b13-M1 <80034669+N00b13-M1@users.noreply.github.com>
2022-05-24 11:04:31 +0200

    build_added_additional_banner_data_for_other_pages_mc

--- database/seeders/BannerSeeder.php
+++ database/seeders/BannerSeeder.php
@@ -34,5 +34,60 @@ class BannerSeeder extends Seeder
             'url_text' => 'Buy now',
             'created_at' =>now(),
         ]);
+        DB::table('banners')->insert([
+            'bg' => 'slidebg-2.jpg',
+            'title' => 'Next year more intresting!<br>Check next',
+            'dropbox' => 'Features',
+            'description' => 'Nunc eget tempor neque. Aenean non ex sed nibh euismod<br>ornare. Nam congue nisi purus, sed luctus risus.',
+            'url'=> '#',
+            'url_text' => 'Buy now',
+            'created_at' =>now(),
+        ]);
+        DB::table('banners')->insert([
+            'bg' => 'page-heading-bg.jpg',
+            'title' => 'ALL COURSES',
+            'dropbox' => '',
+            'description' => 'Salvia next level crucifix pickled heirloom synth',
+            'url'=> 'courses-grid.html',
+            'url_text' => 'Courses Categories',
+            'created_at' =>now(),
+        ]);
+        DB::table('banners')->insert([
+            'bg' => 'events-heading-bg.jpg',
+            'title' => 'EVENTS',
+            'dropbox' => '',
+            'description' => 'Salvia next level crucifix pickled heirloom synth',
+            'url'=> 'grid-news.html',
+            'url_text' => 'News',
+            'created_at' =>now(),
+        ]);
+        DB::table('banners')->insert([
+            'bg' => 'teachers-heading-bg.jpg',
+            'title' => 'OUR TEACHERS',
+            'dropbox' => '',
+            'description' => 'Salvia next level crucifix pickled heirloom synth',
+            'url'=> 'our-teachers.html',
+            'url_text' => 'Our Teachers',
+            'created_at' =>now(),
+        ]);
+        DB::table('banners')->insert([
+            'bg' => 'news-heading-bg.jpg',
+            'title' => 'NEWS',
+            'dropbox' => '',
+            'description' => 'Salvia next level crucifix pickled heirloom synth',
+            'url'=> 'grid-news.html',
+            'url_text' => 'News',
+            'created_at' =>now(),
+        ]);
+        DB::table('banners')->insert([
+            'bg' => 'news-heading-bg.jpg',
+            'title' => 'CONTACT US',
+            'dropbox' => '',
+            'description' => 'Salvia next level crucifix pickled heirloom synth',
+            'url'=> 'contact.html',
+            'url_text' => 'Contact',
+            'created_at' =>now(),
+        ]);
+
     }
 }
