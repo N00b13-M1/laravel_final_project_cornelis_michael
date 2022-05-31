bf94d81f53f699561db3fca6528ee5d6bb259f8e
N00b13-M1 <80034669+N00b13-M1@users.noreply.github.com>
2022-05-31 11:54:49 +0200

    feat_updated_sidebar_front_mc

--- resources/views/front/partials/sidebar-menu.blade.php
+++ resources/views/front/partials/sidebar-menu.blade.php
@@ -3,43 +3,22 @@
         <div class="content">
             <nav class="responsive-menu">
                 <ul>
-                    <li class="menu-item-has-children"><a href="#">Home</a>
-                        <ul class="sub-menu">
-                            <li><a href="index.html">Homepage 1</a></li>
-                            <li><a href="homepage-2.html">Homepage 2</a></li>
-                        </ul>
+                    <li class=""><a href="/">Home</a>
                     </li>
-                    <li class="menu-item-has-children"><a href="#">Courses</a>
-                        <ul class="sub-menu">
-                            <li><a href="courses-grid.html">Courses Grids</a></li>
-                            <li><a href="courses-list.html">Courses List</a></li>
-                            <li><a href="single-course.html">Single Course</a></li>
-                        </ul>
+                    <li class=""><a href="{{ route('courses') }}">Courses</a>
+
                     </li>
-                    <li class="menu-item-has-children"><a href="#">Events</a>
-                        <ul class="sub-menu">
-                            <li><a href="classic-events.html">Classic Events</a></li>
-                            <li><a href="calendar-events.html">Calendar Events</a></li>
-                            <li><a href="single-event.html">Single Event</a></li>
-                        </ul>
+                    <li class=""><a href="{{ route('events') }}">Events</a>
+
                     </li>
-                    <li class="menu-item-has-children"><a href="#">Pages</a>
-                        <ul class="sub-menu">
-                            <li><a href="about.html">About Us</a></li>
-                            <li><a href="our-teachers.html">Our Teachers</a></li>
-                            <li><a href="single-teacher.html">Single Teacher</a></li>
-                            <li><a href="gallery-4.html">Gallery 4 Columns</a></li>
-                            <li><a href="gallery-3.html">Gallery 3 Columns</a></li>
-                        </ul>
-                    </li>
-                    <li class="menu-item-has-children"><a href="#">News</a>
-                        <ul class="sub-menu">
+                    <li class=""><a href="{{ route('news') }}">News</a>
+                        {{-- <ul class="sub-menu">
                             <li><a href="classic-news.html">Classic News</a></li>
                             <li><a href="grid-news.html">Grid News</a></li>
                             <li><a href="single-post.html">Single Post</a></li>
-                        </ul>
+                        </ul> --}}
                     </li>
-                    <li><a href="contact.html">Contact</a></li>
+                    <li><a href="{{ route('contact') }}">Contact</a></li>
                 </ul>
             </nav>
         </div>
